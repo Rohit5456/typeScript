e4a90e5776ba4fdb5c0978e50d9c82a645d91304
Rohit5456 <rohitsanpawar@gmail.com>
2022-11-10 15:02:56 +0530

    Create 01_variable.js
    learn variables in typescript

---
+++ 01_variable.js
@@ -0,0 +1,18 @@
+var n1 = 10;
+console.log(n1);
+var s1 = "The TypeScript Program";
+var s2 = " the powerfull peoples come for powerfull places";
+console.log(s1);
+console.log(s2);
+var a1 = {
+    productId: 1,
+    productName: "iPhone",
+    productPrice: 25000
+};
+console.log(a1);
+//homogenous array
+var arr1 = ['angular', 'javaScript', 'react'];
+console.log(arr1);
+// hetrogenours array
+var arr2 = ['angular', 125455, 'react'];
+console.log(arr2);
