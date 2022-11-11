d07033a23ce9e68e0fa83e9dec80e233d0b33d4f
Rohit5456 <rohitsanpawar@gmail.com>
2022-11-11 20:16:53 +0530

    Create 01_variable.ts

---
+++ 01_variable.ts
@@ -0,0 +1,24 @@
+var n1: number = 10;
+console.log(n1);
+
+var s1: string = "The TypeScript Program";
+var s2: string = " the powerfull peoples come for powerfull places";
+console.log(s1);
+console.log(s2);
+
+
+var a1 : any ={
+    productId : 1,
+    productName : "iPhone",
+    productPrice: 25000,
+}
+console.log(a1);
+
+
+//homogenous array
+var arr1 : Array<string> =['angular','javaScript','react'];
+console.log(arr1);
+
+// hetrogenours array
+var arr2 : Array<any> =['angular',125455,'react'];
+console.log(arr2);
