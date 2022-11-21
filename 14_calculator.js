6e705407b0795df5210a9e84fb51bbdf9f54a8f7
Rohit5456 <rohitsanpawar@gmail.com>
2022-11-21 19:48:57 +0530

    Create 14_calculator.js

---
+++ 14_calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var _14_calc_1 = require("./14_calc");
+var objCalc = new _14_calc_1.Calculator();
+console.log(objCalc.add(2, 3));
+console.log(objCalc.sub(10, 5));
+var courses = ["Angular", "React", "Nodejs"];
+console.log(courses.toString());
+var e1;
+var e2 = { id: 123, name: "John" };
+//e1 = e2 ; // type casting . 
+//e2 = e1; //
